9bdf96c8d44e3789bf514d76e35f5f04cc48cdea
VaradNaik <54986840+VaradNaik@users.noreply.github.com>
2022-12-12 00:30:47 +0530

    correction in findKClosestElements.java

--- medium/binarySearch/FindKClosestElements.java
+++ medium/binarySearch/FindKClosestElements.java
@@ -4,7 +4,7 @@ class Solution {
         int left = 0;
         int right = arr.length - k;
 
-        while(left <= right){
+        while(left < right){
             int mid = left + (right - left)/2;
             if(x - arr[mid] > arr[mid+k] - x){
                 left = mid+1;
