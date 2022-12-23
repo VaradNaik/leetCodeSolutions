c89183e1f984416c97b8c5bac72d7e2646336ced
VaradNaik <54986840+VaradNaik@users.noreply.github.com>
2022-12-23 14:46:54 +0530

    MinimumKnightMoves.java

---
+++ medium/dynamicProgramming/MinimumKnightMoves.java
@@ -0,0 +1,26 @@
+class Solution {
+    Map<String,Integer> memo = new HashMap();
+
+    public int dfs(int x,int y){
+        String key = x + "," + y;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        if(x+y==0){
+            return 0;
+        }
+        if(x+y == 2){
+            return 2;
+        }
+        else {
+            int ret = Math.min(dfs(Math.abs(x-1),Math.abs(y-2)), dfs(Math.abs(x-2),Math.abs(y-1))) + 1;
+            memo.put(key,ret);
+            return ret;
+        }
+    }
+
+
+    public int minKnightMoves(int x, int y) {
+        return dfs(Math.abs(x),Math.abs(y));
+    }
+}
