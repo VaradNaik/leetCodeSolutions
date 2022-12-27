6bab2b8770ee578a452301166a93310c95823d19
VaradNaik <54986840+VaradNaik@users.noreply.github.com>
2022-12-27 18:56:45 +0530

    refactored codebase by putting solution classes in appropriate directories

--- medium/stacks/DecodeString.java
+++ medium/stacks/DecodeString.java
@@ -4,25 +4,31 @@ class Solution {
         Stack<StringBuilder> stringStack = new Stack<>();
         Stack<Integer> kStack = new Stack<>();
 
-        StringBuilder currentString = new StringBuilder();
-
+        StringBuilder currentStringBuilder = new StringBuilder();
+        int k =0;
         for(char ch: s.toCharArray()){
             if(Character.isDigit(ch)){
                 k = k * 10 + (ch - '0');
             }
             else if(ch == '['){
                 kStack.push(k);
-                stringStack.push(currentString);
-                currentString = new StringBuilder();
+                stringStack.push(currentStringBuilder);
+                currentStringBuilder = new StringBuilder();
                 k=0;
             }
             else if(ch == ']'){
-                StringBuilder decded = stringStack.pop();
+                StringBuilder decoded = stringStack.pop();
 
                 for(int j = kStack.pop();j>0;j--){
-                    decoded =
+                    decoded.append(currentStringBuilder);
                 }
+
+                currentStringBuilder = decoded;
+            }
+            else{
+                currentStringBuilder.append(ch);
             }
         }
+        return currentStringBuilder.toString();
     }
 }
